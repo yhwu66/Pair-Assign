2514612b1a59199ee7030375482e3a40cb3611fb
yhwu66 <97576976+yhwu66@users.noreply.github.com>
2023-09-21 15:23:55 -0700

    fix bug

--- Assets/Scripts/SpaceshipCollision.cs
+++ Assets/Scripts/SpaceshipCollision.cs
@@ -4,8 +4,18 @@ public class SpaceshipCollision : MonoBehaviour
 {
     public WordManager wordManager;
     public TextPopupController popupController;
+    private float collisionCooldown = 0.2f;  // Duration for cooldown
+    private float lastCollisionTime = -10.0f;  // Time of the last collision
+    
+    
     private void OnCollisionEnter(Collision collision) 
     {
+        if (Time.time - lastCollisionTime < collisionCooldown) 
+        {
+            return;  // Ignore this collision as it's too close to the last one
+        }
+
+        lastCollisionTime = Time.time;  // Update the time of the latest collision
     
         if (collision.gameObject.tag.StartsWith("letter_"))
         {
