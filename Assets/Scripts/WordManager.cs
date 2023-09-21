3e80a9b1b7c5d03b0054dc25a54aa5a7f8ccb712
yhwu66 <97576976+yhwu66@users.noreply.github.com>
2023-09-20 19:13:00 -0700

    add current word

--- Assets/Scripts/WordManager.cs
+++ Assets/Scripts/WordManager.cs
@@ -1,17 +1,36 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class WordManager : MonoBehaviour
 {
-    public List<string> words = new List<string>() { "cat", "dog"};
+    public List<string> words = new List<string>() {"cat"};
     public int currentWordIndex = 0;
+    public Text displayTextCurrentWord;
+    public Text displayTextTimer;
+    public Text displayTextWin;
 
     private HashSet<char> currentWordSet;
+    private float startTime;
 
     public void Start()
     {
         Debug.Log("Current word: " +  words[0]);
         currentWordSet = new HashSet<char>(words[0]);
+        displayTextCurrentWord.text = "Current Word is: "+ "'" + words[0] + "'";
+        //displayTextWin.text = "You Win!";
+        displayTextWin.enabled = false;
+        displayTextTimer.enabled = false;
+        startTime = Time.time;
+        
+    }
+
+    public void Update()
+    {
+        float t = Time.time - startTime;
+        string seconds = (t % 60).ToString("f2");
+        //displayTextTimer.text = "Time: " + seconds + "s";
     }
 
 
@@ -40,16 +59,19 @@ public class WordManager : MonoBehaviour
     {
         Debug.Log("Word Completed: " + words[currentWordIndex]);
         currentWordIndex++;
+        
 
         // Check if we have more words
         if (currentWordIndex < words.Count){
             Debug.Log("Current word: " +  words[currentWordIndex]);
             currentWordSet = new HashSet<char>(words[currentWordIndex]);
+            displayTextCurrentWord.text = "Current Word is: "+ "'" + words[currentWordIndex] + "'";
         }
         else
         {
             Debug.Log("All words completed! You win!");
-            
+            //displayTextWin.enabled = true;
         }
     }
+    
 }
