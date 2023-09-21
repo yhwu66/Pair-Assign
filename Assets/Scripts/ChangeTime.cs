f0d07c20c027651a748c4fba03502bda3587c02c
SHARAN MURLI <murli@usc.edu>
2023-09-20 19:35:16 -0700

    update game over scene

--- Assets/Scripts/ChangeTime.cs
+++ Assets/Scripts/ChangeTime.cs
@@ -1,11 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeTime : MonoBehaviour
 {
-private float DelayBeforeLoading=10f;
+private float DelayBeforeLoading = 10f;
 
 private string sceneNameToLoad;
 private float timeElapsed;
@@ -14,6 +13,7 @@ private float timeElapsed;
   private void Update()
     {
         timeElapsed += Time.deltaTime;
+
         if (timeElapsed > DelayBeforeLoading)
         {
             SceneManager.LoadScene(sceneNameToLoad);
